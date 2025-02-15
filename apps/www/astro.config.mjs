9032426f9254ba8f8fdaa90dde7b5cc00983751a
Felix Eklöf <felixeklof@icloud.com>
2025-02-15 14:07:45 +0100

    astro site

--- apps/www/astro.config.mjs
+++ apps/www/astro.config.mjs
@@ -1,5 +1,18 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import astroTypesafeRoutes from "astro-typesafe-routes";
+import * as path from "node:path";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [astroTypesafeRoutes()],
+  vite: {
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  },
+});
