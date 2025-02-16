cfe7fee4a1700d1edd40c33865b059750d5d71f7
Felix Eklöf <felixeklof@icloud.com>
2025-02-16 13:01:32 +0100

    use import meta

--- apps/www/astro.config.mjs
+++ apps/www/astro.config.mjs
@@ -12,7 +12,7 @@ export default defineConfig({
     assetsInclude: ["**/*.txt"],
     resolve: {
       alias: {
-        "@": path.resolve(__dirname, "src"),
+        "@": path.resolve(import.meta.dir, "src"),
       },
     },
   },
